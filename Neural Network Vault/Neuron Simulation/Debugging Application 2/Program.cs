146a094299b699eda7fbf127b7ec7d6857e03eb0
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-02-04 13:45:44 -0600

    End of day push
    done for this week...

--- Neural Network Vault/Neuron Simulation/Debugging Application 2/Program.cs
+++ Neural Network Vault/Neuron Simulation/Debugging Application 2/Program.cs
@@ -2,9 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading;
-using static NeuralNetworkFundamentals.NeuralNetwork;
 
 namespace Debugging_Application_2
 {
@@ -21,12 +18,13 @@ namespace Debugging_Application_2
         {
             Console.WriteLine("Setting up test...");
             iterations = 2000;
-            List<int> layerInfo = new List<int>() { 2, 2, 1 };
+            List<int> layerInfo = new List<int>() { 2, 2, 2 };
+            Random rnd = new Random();
 
-            NeuralNetwork net = new NeuralNetwork(layerInfo, learningRate: 0.5);   // Creates a network with 2 inputs, 1 hidden layer of 2, and 2 outputs
+            NeuralNetwork net = new NeuralNetwork(layerInfo, learningRate: 0.5, momentum: rnd.NextDouble());   // Creates a network with 2 inputs, 1 hidden layer of 2, and 2 outputs
 
             // Sets the weights and biases of the network prior to training
-            /*
+            
             net.Biases = new List<List<double>>()
             {
                 new List<double>(){0,0 },
@@ -47,33 +45,33 @@ namespace Debugging_Application_2
                     new List<double>(){0.5, 0.55 }
                 }
             };
-            */
-            net.GenWeightsAndBiases();
+            
+            //net.GenWeightsAndBiases();
 
             net.TrainingUpdateEvent += OnTrainingUpdateEvent;
             net.TrainingFinishEvent += OnTrainingFinishEvent;
 
             // Creates the samples and outputs
-            
+            /*
             sampleIn = new List<List<double>>()
             {
                 new List<double>(){0, 0 },
-                new List<double>(){1, 1 },
                 new List<double>(){0, 1 },
-                new List<double>(){1, 0 }
+                new List<double>(){1, 0 },
+                new List<double>(){1, 1 }
             };
 
             sampleOut = new List<List<double>>()
             {
                 new List<double>(){0},
-                new List<double>(){0},
                 new List<double>(){1},
-                new List<double>(){1}
+                new List<double>(){1},
+                new List<double>(){0}
             };
-            
+            */
 
-            //sampleIn = new List<List<double>>() { new List<double>() { 0.05, 0.1 } };
-            //sampleOut = new List<List<double>>() { new List<double>() { 0.01, 0.99 } };
+            sampleIn = new List<List<double>>() { new List<double>() { 0.05, 0.1 } };
+            sampleOut = new List<List<double>>() { new List<double>() { 0.01, 0.99 } };
 
             // Trains the network
             IsTraining = true;
@@ -87,7 +85,7 @@ namespace Debugging_Application_2
             {
                 List<double> temp = net.Calc(sample);
                 Console.WriteLine("For Inputs [{0}, {1}]:", sample[0], sample[1]);
-                Console.WriteLine("The output is: {0}\n", temp[0]);
+                Console.WriteLine("The output is: {0}, {1}\n", temp[0], temp[1]);
             }
 
             Console.ReadKey();
@@ -97,6 +95,7 @@ namespace Debugging_Application_2
         {
             // Executes every time the network finishes a training sample
             Console.WriteLine("Finished Iteration " + result.Iteration);
+            Console.WriteLine("Sample [{0}, {1}] XOR [{2}]", result.Layers[0][0], result.Layers[0][1], result.Layers.Last()[0].Activation);
             //Console.WriteLine("Error: {0}", result.Error);
         }
 
