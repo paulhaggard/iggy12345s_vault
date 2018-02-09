8e4a35d0392081b5d62cbd504046e1ae0dc4448c
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-02-09 16:33:32 -0600

    Added threading dll
    Created a threading dll class library that will help simplify the process of launching asynchronous modules that can communicate interdependently with each other

---
+++ ThreadHelper dll/ThreadHelper Library/ThreadHelper Library/TSubject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThreadHelper_Library
+{
+    public interface TSubject
+    {
+        /* This interface is to be a parent to any class that you want to be executed on the separate thread.
+         * It provides a method for the ThreadCaller class to execute when the process should be launched.
+         */
+        void Start();
+    }
+}
---
+++ ThreadHelper dll/ThreadHelper Library/ThreadHelper Library/ThreadLauncher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ThreadHelper_Library
+{
+    class ThreadLauncher<T>
+    {
+        // Contains and launches threads as told to.
+        List<Thread> Threads;   // List of threads to be launched.
+        bool isLaunched;        // Used to tell when the threads have been launched
+        Queue<T> CommQueue;     // Queue used for callback communication
+
+    }
+}
