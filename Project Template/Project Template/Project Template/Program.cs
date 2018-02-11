d49c825542b7b1b552b461449133fcae911dc28d
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-02-11 01:01:41 -0600

    Added Project Template
    Created a project template that will incorporate the use of the threadhelper dll and will help streamline code layout

---
+++ Project Template/Project Template/Project Template/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using ThreadHelper_Library;
+
+namespace Project_Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting System!");
+            ThreadLauncher<CommQueueDefaultData> ThreadHandler = new ThreadLauncher<CommQueueDefaultData>();    // Initializes the thread handler
+            List<TSubject<CommQueueDefaultData>> Modules = new List<TSubject<CommQueueDefaultData>>();          // Creates an instance of the module list
+            /* 
+             * Add your custom modules to launch asynchronously here.
+             */
+
+                                                                                                                // Adds all of the modules to the Handler
+            foreach (TSubject<CommQueueDefaultData> item in Modules)
+                ThreadHandler.Add(item);
+
+            Console.WriteLine("Preparing to launch modules...");
+            
+
+        }
+    }
+}
