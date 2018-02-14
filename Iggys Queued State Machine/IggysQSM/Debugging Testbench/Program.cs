a8a933316e6febeca3a379c8877f1a866c8e1430
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-02-13 21:43:19 -0600

    Tested QSM project
    Tested the QSM project to verify that it was working properly.

---
+++ Iggys Queued State Machine/IggysQSM/Debugging Testbench/Program.cs
@@ -0,0 +1,19 @@
+﻿using IggysQSM;
+using System;
+
+namespace Debugging_Testbench
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            QSM qsm = new QSM();
+            bool isExitting = false;
+            while(!isExitting)
+            {
+                Console.WriteLine("The next state is: {0}", qsm.GetNextState().State);
+            }
+        }
+    }
+}
