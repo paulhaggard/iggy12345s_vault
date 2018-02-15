7cbc0c5859b3cfadedbb6d8fc26168b66152cccf
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-02-15 09:50:31 -0600

    Added RPG Character creator Resource Engine
    This engine will be responsible for streamlining the xml file generation process

---
+++ RPG character creator/RPGCharacterGeneratorResourceEngine/RPGCharacterGeneratorResourceEngine/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace RPGCharacterGeneratorResourceEngine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.raceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.classToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.spellToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.weaponToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clothingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.armorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.jewelryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.normalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.RaceList = new System.Windows.Forms.ListBox();
+            this.ClassList = new System.Windows.Forms.ListBox();
+            this.SpellList = new System.Windows.Forms.ListBox();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.propertiesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.removeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1138, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveToolStripMenuItem1,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem1,
+            this.raceToolStripMenuItem,
+            this.classToolStripMenuItem,
+            this.spellToolStripMenuItem,
+            this.weaponToolStripMenuItem,
+            this.clothingToolStripMenuItem,
+            this.itemToolStripMenuItem});
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // raceToolStripMenuItem
+            // 
+            this.raceToolStripMenuItem.Name = "raceToolStripMenuItem";
+            this.raceToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.raceToolStripMenuItem.Text = "Race";
+            // 
+            // classToolStripMenuItem
+            // 
+            this.classToolStripMenuItem.Name = "classToolStripMenuItem";
+            this.classToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.classToolStripMenuItem.Text = "Class";
+            // 
+            // spellToolStripMenuItem
+            // 
+            this.spellToolStripMenuItem.Name = "spellToolStripMenuItem";
+            this.spellToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.spellToolStripMenuItem.Text = "Spell";
+            // 
+            // weaponToolStripMenuItem
+            // 
+            this.weaponToolStripMenuItem.Name = "weaponToolStripMenuItem";
+            this.weaponToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.weaponToolStripMenuItem.Text = "Weapon";
+            // 
+            // clothingToolStripMenuItem
+            // 
+            this.clothingToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.armorToolStripMenuItem,
+            this.jewelryToolStripMenuItem,
+            this.normalToolStripMenuItem});
+            this.clothingToolStripMenuItem.Name = "clothingToolStripMenuItem";
+            this.clothingToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.clothingToolStripMenuItem.Text = "Clothing";
+            // 
+            // armorToolStripMenuItem
+            // 
+            this.armorToolStripMenuItem.Name = "armorToolStripMenuItem";
+            this.armorToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.armorToolStripMenuItem.Text = "Armor";
+            // 
+            // jewelryToolStripMenuItem
+            // 
+            this.jewelryToolStripMenuItem.Name = "jewelryToolStripMenuItem";
+            this.jewelryToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.jewelryToolStripMenuItem.Text = "Jewelry";
+            // 
+            // normalToolStripMenuItem
+            // 
+            this.normalToolStripMenuItem.Name = "normalToolStripMenuItem";
+            this.normalToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.normalToolStripMenuItem.Text = "Normal";
+            // 
+            // itemToolStripMenuItem
+            // 
+            this.itemToolStripMenuItem.Name = "itemToolStripMenuItem";
+            this.itemToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.itemToolStripMenuItem.Text = "Item";
+            // 
+            // RaceList
+            // 
+            this.RaceList.FormattingEnabled = true;
+            this.RaceList.Location = new System.Drawing.Point(12, 27);
+            this.RaceList.Name = "RaceList";
+            this.RaceList.Size = new System.Drawing.Size(115, 511);
+            this.RaceList.TabIndex = 1;
+            // 
+            // ClassList
+            // 
+            this.ClassList.FormattingEnabled = true;
+            this.ClassList.Location = new System.Drawing.Point(133, 27);
+            this.ClassList.Name = "ClassList";
+            this.ClassList.Size = new System.Drawing.Size(115, 511);
+            this.ClassList.TabIndex = 1;
+            // 
+            // SpellList
+            // 
+            this.SpellList.FormattingEnabled = true;
+            this.SpellList.Location = new System.Drawing.Point(254, 27);
+            this.SpellList.Name = "SpellList";
+            this.SpellList.Size = new System.Drawing.Size(131, 511);
+            this.SpellList.TabIndex = 1;
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Load";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.propertiesToolStripMenuItem,
+            this.removeToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // propertiesToolStripMenuItem
+            // 
+            this.propertiesToolStripMenuItem.Name = "propertiesToolStripMenuItem";
+            this.propertiesToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.propertiesToolStripMenuItem.Text = "Properties";
+            // 
+            // removeToolStripMenuItem
+            // 
+            this.removeToolStripMenuItem.Name = "removeToolStripMenuItem";
+            this.removeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.removeToolStripMenuItem.Text = "Remove";
+            // 
+            // saveToolStripMenuItem1
+            // 
+            this.saveToolStripMenuItem1.Name = "saveToolStripMenuItem1";
+            this.saveToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem1.Text = "Save";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            // 
+            // fileToolStripMenuItem1
+            // 
+            this.fileToolStripMenuItem1.Name = "fileToolStripMenuItem1";
+            this.fileToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.fileToolStripMenuItem1.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1138, 543);
+            this.Controls.Add(this.SpellList);
+            this.Controls.Add(this.ClassList);
+            this.Controls.Add(this.RaceList);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem raceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem classToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem spellToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem weaponToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clothingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem armorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem jewelryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem normalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem itemToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem propertiesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem removeToolStripMenuItem;
+        private System.Windows.Forms.ListBox RaceList;
+        private System.Windows.Forms.ListBox ClassList;
+        private System.Windows.Forms.ListBox SpellList;
+    }
+}
