80914dc206db2188648a5ca58f74f25f7dcaab70
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-02-16 23:30:48 -0600

    Added PCGen Plugin projects
    Added projects meant for interfacing with the rpg character generator PCGen

---
+++ RPG character creator/PCGenPluginLibrary/PCGenCharacter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PCGenPluginLibrary
+{
+    class PCGenCharacter : PCGenFile
+    {
+        private List<string> headers;
+        private List<List<string>> sectionInfo;
+
+        public PCGenCharacter(string filepath) : base(filepath)
+        {
+            headers = getHeaderList();
+            sectionInfo = getSectionInfo();
+        }
+
+        public override void GenObject()
+        {
+            
+        }
+    }
+}
