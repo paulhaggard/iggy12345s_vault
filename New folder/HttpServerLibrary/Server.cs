d445eec136f273cdada1002d3787427ab2fd2a2b
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-02-19 22:50:19 -0600

    Added http server library
    Added a simple http server library that helps with handling http requests.

---
+++ New folder/HttpServerLibrary/Server.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Collections.Generic;
+using System.IO;
+
+namespace HttpServerLibrary
+{
+    public class Server
+    {
+        // A simple server class used to host an http server
+        // Referenced from here: https://gist.github.com/aksakalli/9191056
+
+        private readonly string[] _indexFiles = {
+        "index.html",
+        "index.htm",
+        "default.html",
+        "default.htm"
+        };
+
+        private static IDictionary<string, string> _mimeTypeMappings = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase) {
+        #region extension to MIME type list
+        {".asf", "video/x-ms-asf"},
+        {".asx", "video/x-ms-asf"},
+        {".avi", "video/x-msvideo"},
+        {".bin", "application/octet-stream"},
+        {".cco", "application/x-cocoa"},
+        {".crt", "application/x-x509-ca-cert"},
+        {".css", "text/css"},
+        {".deb", "application/octet-stream"},
+        {".der", "application/x-x509-ca-cert"},
+        {".dll", "application/octet-stream"},
+        {".dmg", "application/octet-stream"},
+        {".ear", "application/java-archive"},
+        {".eot", "application/octet-stream"},
+        {".exe", "application/octet-stream"},
+        {".flv", "video/x-flv"},
+        {".gif", "image/gif"},
+        {".hqx", "application/mac-binhex40"},
+        {".htc", "text/x-component"},
+        {".htm", "text/html"},
+        {".html", "text/html"},
+        {".ico", "image/x-icon"},
+        {".img", "application/octet-stream"},
+        {".iso", "application/octet-stream"},
+        {".jar", "application/java-archive"},
+        {".jardiff", "application/x-java-archive-diff"},
+        {".jng", "image/x-jng"},
+        {".jnlp", "application/x-java-jnlp-file"},
+        {".jpeg", "image/jpeg"},
+        {".jpg", "image/jpeg"},
+        {".js", "application/x-javascript"},
+        {".mml", "text/mathml"},
+        {".mng", "video/x-mng"},
+        {".mov", "video/quicktime"},
+        {".mp3", "audio/mpeg"},
+        {".mpeg", "video/mpeg"},
+        {".mpg", "video/mpeg"},
+        {".msi", "application/octet-stream"},
+        {".msm", "application/octet-stream"},
+        {".msp", "application/octet-stream"},
+        {".pdb", "application/x-pilot"},
+        {".pdf", "application/pdf"},
+        {".pem", "application/x-x509-ca-cert"},
+        {".pl", "application/x-perl"},
+        {".pm", "application/x-perl"},
+        {".png", "image/png"},
+        {".prc", "application/x-pilot"},
+        {".ra", "audio/x-realaudio"},
+        {".rar", "application/x-rar-compressed"},
+        {".rpm", "application/x-redhat-package-manager"},
+        {".rss", "text/xml"},
+        {".run", "application/x-makeself"},
+        {".sea", "application/x-sea"},
+        {".shtml", "text/html"},
+        {".sit", "application/x-stuffit"},
+        {".swf", "application/x-shockwave-flash"},
+        {".tcl", "application/x-tcl"},
+        {".tk", "application/x-tcl"},
+        {".txt", "text/plain"},
+        {".war", "application/java-archive"},
+        {".wbmp", "image/vnd.wap.wbmp"},
+        {".wmv", "video/x-ms-wmv"},
+        {".xml", "text/xml"},
+        {".xpi", "application/x-xpinstall"},
+        {".zip", "application/zip"},
+        #endregion
+        };
+
+        private string _rootDirectory;
+        private int _port;
+        private Thread _serverRxThread;
+        private Thread _serverProcessThread;
+        private HttpListener _listener;
+        private bool isExitting;
+        private bool isListening;
+        private Queue<HttpListenerContext> RxQueue;
+        
+        public Server(string path)
+        {
+            // Retrieves an empty socket
+            TcpListener l = new TcpListener(IPAddress.Loopback, 0);
+            l.Start();
+            int port = ((IPEndPoint)l.LocalEndpoint).Port;  // the auto assigned port from the router
+            l.Stop();
+            Initialize(path, port);
+        }
+
+        public Server(string path, int port)
+        {
+            Initialize(path, port);
+        }
+
+        public void Stop()
+        {
+            isExitting = true;
+            _serverProcessThread.Abort();
+            _serverRxThread.Abort();
+            _listener.Stop();
+        }
+
+        private void Listen()
+        {
+            _listener = new HttpListener();
+            string prefix = "http://*:" + _port.ToString() + "/";
+            _listener.Prefixes.Add(prefix);
+            _listener.Start();
+            Console.WriteLine("Listening on: {0}", prefix);
+
+            while(!isExitting)
+            {
+                try
+                {
+                    HttpListenerContext context = _listener.GetContext();
+                    RxQueue.Enqueue(context);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Server exception: {0}", e.Message);
+                }
+            }
+
+        }
+
+        private void MessageHandler()
+        {
+            // Monitors the queue for incoming requests and handles them.
+            Console.WriteLine("Awaiting messages");
+            while(!isExitting)
+            {
+                if(RxQueue.Count > 0)
+                {
+                    Process(RxQueue.Dequeue());
+                }
+            }
+        }
+
+        protected virtual void Process(HttpListenerContext context)
+        {
+            // Used to process requests from the clients
+            string filename = context.Request.Url.AbsolutePath;
+            Console.WriteLine(filename);
+            filename = filename.Substring(1);
+
+            if (string.IsNullOrEmpty(filename))
+            {
+                foreach (string indexFile in _indexFiles)
+                {
+                    if (File.Exists(Path.Combine(_rootDirectory, indexFile)))
+                    {
+                        filename = indexFile;
+                        break;
+                    }
+                }
+            }
+
+            filename = Path.Combine(_rootDirectory, filename);
+
+            if (File.Exists(filename))
+            {
+                try
+                {
+                    Stream input = new FileStream(filename, FileMode.Open);
+
+                    //Adding permanent http response headers
+                    string mime;
+                    context.Response.ContentType = _mimeTypeMappings.TryGetValue(Path.GetExtension(filename), out mime) ? mime : "application/octet-stream";
+                    context.Response.ContentLength64 = input.Length;
+                    context.Response.AddHeader("Date", DateTime.Now.ToString("r"));
+                    context.Response.AddHeader("Last-Modified", System.IO.File.GetLastWriteTime(filename).ToString("r"));
+
+                    byte[] buffer = new byte[1024 * 16];
+                    int nbytes;
+                    while ((nbytes = input.Read(buffer, 0, buffer.Length)) > 0)
+                        context.Response.OutputStream.Write(buffer, 0, nbytes);
+                    input.Close();
+
+                    context.Response.StatusCode = (int)HttpStatusCode.OK;
+                    context.Response.OutputStream.Flush();
+                }
+                catch (Exception ex)
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                }
+
+            }
+            else
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+            }
+
+            context.Response.OutputStream.Close();
+        }
+
+        private void Initialize(string path, int port)
+        {
+            isExitting = false;
+            RxQueue = new Queue<HttpListenerContext>();
+            Console.WriteLine("Starting Server at {0} on port {1}...", path, port);
+            _rootDirectory = path;
+            _port = port;
+            _serverRxThread = new Thread(Listen);
+            _serverProcessThread = new Thread(MessageHandler);
+            _serverProcessThread.Start();
+            _serverRxThread.Start();
+        }
+    }
+}
