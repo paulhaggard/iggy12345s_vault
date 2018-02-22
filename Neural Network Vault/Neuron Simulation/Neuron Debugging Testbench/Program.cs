e9a5036fcf44b6ff1e4691bb9c6ef6d244f8871c
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-02-21 20:50:33 -0600

    Added a neuron debugging testbench
    Added a small program used to verify the functionality of individual neurons

---
+++ Neural Network Vault/Neuron Simulation/Neuron Debugging Testbench/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Neuron_Debugging_Testbench
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
