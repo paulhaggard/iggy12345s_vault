5f887cb1d2ca7948b2d3c04bf0cdb4c186add9cb
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-02-21 22:19:10 -0600

    Debugged the neuron class for convergence
    Debugged that the neuron class is indeed functioning correctly by passing in a known value and expected output and verifying that it converges on the value desired.

--- Neural Network Vault/Neuron Simulation/Neuron Debugging Testbench/Program.cs
+++ Neural Network Vault/Neuron Simulation/Neuron Debugging Testbench/Program.cs
@@ -1,12 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
+using NeuralNetworkFundamentals;
+using static NeuralNetworkFundamentals.Neuron;
 
 namespace Neuron_Debugging_Testbench
 {
     class Program
     {
+        static bool activationFlag;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Creates the input to the test neuron
+            Neuron neuronTestInput = new Neuron();
+            // Creates an output layer neuron
+            Neuron neuronTest = new Neuron(new Neuron[] { neuronTestInput },
+                new List<double>() { 0.1 }, 
+                outputLayer: true);
+
+            neuronTest.ActiveEvent += OnActivate;
+
+            Random rnd = new Random();
+
+            // Our testing sample values
+            double input = 0.05;
+            double expectedOutput = 0.6;
+            char key;
+
+            do
+            {
+                // Prepare the test
+                activationFlag = true;
+                neuronTestInput.RawInput = input;
+                neuronTestInput.Activate(); // Activate the input
+                while (activationFlag) ;    // Wait for the activation to finish.
+
+                checkError();
+
+                key = Console.ReadKey().KeyChar;
+
+            } while (key != 's');
+
+            void checkError()
+            {
+                // Checks and reports the error of the neuron to the console
+                Console.WriteLine("The output of the neuron is {0} and should be {1}", neuronTest.Activation, expectedOutput);
+                neuronTest.AssignError(1, 0.5, expectedOutput, AdjustValues: false);    // Calculates the derivatives of the neuron's error
+                Console.WriteLine("The error of the neuron was {0}", neuronTest.Error);
+                double prevBias = neuronTest.Bias;
+                double prevWeight = neuronTest.Weights[0];
+                neuronTest.AdjustValues(1, 0.5, expectedOutput);                        // Assigns the gradient to the weight and bias.
+                Console.WriteLine("The Bias was changed by a factor of {0} to {1} from {2}", prevBias - neuronTest.Bias, neuronTest.Bias, prevBias);
+                Console.WriteLine("The Weight was changed by a factor of {0} to {1} from {2}", prevWeight - neuronTest.Weights[0], neuronTest.Weights[0], prevWeight);
+            }
+        }
+
+        static void OnActivate(object sender, ActivationEventArgs e)
+        {
+            //Neuron temp = (Neuron)sender;
+            Console.WriteLine("The output for the neuron is {0} when the input is {1}", e.Activation, e.Input);
+            activationFlag = false;
         }
     }
 }
