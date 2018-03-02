f0e3fbbe1c0c9d31dec95894f6e2b5296ef4793c
BuildTools <unconfigured@null.spigotmc.org>
2018-03-02 09:10:06 -0600

    fixed the namespace
    however that broke everything

--- Neural Network Vault/Neuron Simulation/neuralSimGui/Form1.cs
+++ Neural Network Vault/Neuron Simulation/neuralSimGui/Form1.cs
@@ -9,7 +9,7 @@ using System.Threading;
 using System.Windows.Forms;
 using NeuralNetworkFundamentals;
 
-namespace Neuron_Simulation
+namespace neuralSimGui
 {
     public partial class Form1 : Form
     {
