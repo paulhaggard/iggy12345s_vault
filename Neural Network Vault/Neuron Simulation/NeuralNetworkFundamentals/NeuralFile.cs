8a824b8a51d70601db63546a457fe642435027b6
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-03-02 09:00:40 -0600

    Created File class

---
+++ Neural Network Vault/Neuron Simulation/NeuralNetworkFundamentals/NeuralFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworkFundamentals
+{
+    class NeuralFile
+    {
+        // A class for reading in the weights and biases into a network from a file.
+        
+    }
+}
