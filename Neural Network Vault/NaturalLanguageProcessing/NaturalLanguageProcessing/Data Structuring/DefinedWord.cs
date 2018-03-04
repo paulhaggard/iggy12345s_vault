7b63e1b5035d284b07346756e49b6ce07955e12b
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-03-03 21:16:28 -0600

    Created Word Class
    Created a word class to use in the dictionary of the language processor.

---
+++ Neural Network Vault/NaturalLanguageProcessing/NaturalLanguageProcessing/Data Structuring/DefinedWord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NaturalLanguageProcessing
+{
+    public abstract class DefinedWord
+    {
+        // This is a class of word that has a definition picked out, by defualt it's zero (the first one)
+        // It has a method called define, that seeks out and returns the longest single lined definition it can find of it's word, using it's definition.
+        private int definitionIndex;
+        private Word word;
+
+        public int DefinitionIndex { get => definitionIndex; set => definitionIndex = value; }
+        public Word Word { get => word; set => word = value; }
+
+        public DefinedWord(Word word, int index = 0)
+        {
+            definitionIndex = index;
+            this.word = word;
+        }
+
+        public abstract void Define();
+    }
+}
---
+++ Neural Network Vault/NaturalLanguageProcessing/NaturalLanguageProcessing/Data Structuring/Word.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NaturalLanguageProcessing
+{
+    // This is a list of all word classes that exist, this helps determine what kind of word is being used.
+    public enum WordClasses
+    {
+        Noun = 0, Verb, Adjective, Adverb, Pronoun, Preposition, Conjunction,
+        Determiner, Exclamation
+    }
+
+    public class Word
+    {
+        // This class is used to define words that come from either the user, or wordnet
+        // The contain the name of the word, it's word type, and it's definitions.
+
+        // Properties
+        private string name;
+        private List<WordClasses> types;
+        private List<List<DefinedWord>> definitions;
+
+        // Accessor Methods
+        public string Name { get => name; set => name = value; }
+        public List<WordClasses> Types { get => types; set => types = value; }
+        public List<List<DefinedWord>> Definitions { get => definitions; set => definitions = value; }
+
+        // Constructor
+        public Word(string name, List<WordClasses> types = null, List<List<DefinedWord>> definitions = null)
+        {
+            this.name = name;
+            this.types = types ?? new List<WordClasses>();
+            this.definitions = definitions ?? new List<List<DefinedWord>>();
+        }
+
+        // Method used for finding a definition of a word.
+        public virtual void Define(int index = 0)
+        {
+            foreach(DefinedWord word in definitions[index])
+            {
+                word.Define();
+            }
+        }
+    }
+}
