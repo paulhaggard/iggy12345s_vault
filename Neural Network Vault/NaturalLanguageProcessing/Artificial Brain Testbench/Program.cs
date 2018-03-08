0a54028322214348e42e28ea6433ee78432ee2d7
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-03-08 08:39:46 -0600

    Added a Testing Project
    Created a testing project for the artificial brain library.

---
+++ Neural Network Vault/NaturalLanguageProcessing/Artificial Brain Testbench/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Artificial_Brain_Testbench
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
