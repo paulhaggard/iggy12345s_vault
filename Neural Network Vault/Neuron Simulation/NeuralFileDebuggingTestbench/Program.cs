7c609a256e038fd92ea75e02bbda6e0018916b3c
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-03-26 09:58:07 -0500

    Added a debugging project for the NeuralFile class

---
+++ Neural Network Vault/Neuron Simulation/NeuralFileDebuggingTestbench/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NeuralFileDebuggingTestbench
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
