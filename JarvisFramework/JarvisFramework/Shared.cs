af64168a6ef5257bf52aefcd3d01aebadb37de71
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-06-30 11:09:13 -0500

    Worked on the Jarvis Framework

--- JarvisFramework/JarvisFramework/Shared.cs
+++ JarvisFramework/JarvisFramework/Shared.cs
@@ -32,4 +32,51 @@ namespace JarvisFramework
 
         public string Message { get => message; set => message = value; }
     }
+
+    public class CommandParser
+    {
+        /// <summary>
+        /// Parses commands out of the command string sent in.
+        /// </summary>
+        /// <param name="commandString">String of commands to be parsed</param>
+        /// <returns>Returns a list of command clusters that were parsed out of the string</returns>
+        public List<CommandCluster> ParseCommands(string commandString)
+        {
+            List<char> charList = commandString.ToList();
+            return new List<CommandCluster>();
+        }
+    }
+
+    /// <summary>
+    /// Cluster used to house commands sent to Jarvis
+    /// </summary>
+    public class CommandCluster
+    {
+        #region Properties
+
+        private string command;
+        private object data;
+
+        #endregion
+
+        public CommandCluster(string command, object data = null)
+        {
+            this.command = command;
+            this.data = data;
+        }
+
+        #region Accessor Methods
+
+        /// <summary>
+        /// Command to be executed
+        /// </summary>
+        public string Command { get => command; set => command = value; }
+
+        /// <summary>
+        /// Data sent with the command
+        /// </summary>
+        public object Data { get => data; set => data = value; }
+
+        #endregion
+    }
 }
