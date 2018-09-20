0382b0a7953c24c9b729b2e624b6e86dcf991a80
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-09-20 14:01:18 -0500

    Started working on a sprite editor for gamemaking

---
+++ GameMakingKit/SpriteEditor/SpriteDesigner.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace SpriteEditor
+{
+    partial class SpriteDesigner
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SpriteDesigner));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.spriteSizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonPencil = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonEraser = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.pictureBoxCanvas = new System.Windows.Forms.PictureBox();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.canvasVScrollBar = new System.Windows.Forms.VScrollBar();
+            this.canvasHScrollBar = new System.Windows.Forms.HScrollBar();
+            this.LayerList = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBoxPreview = new System.Windows.Forms.PictureBox();
+            this.listViewLayers = new System.Windows.Forms.ListView();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCanvas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.spriteSizeToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // spriteSizeToolStripMenuItem
+            // 
+            this.spriteSizeToolStripMenuItem.Name = "spriteSizeToolStripMenuItem";
+            this.spriteSizeToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.spriteSizeToolStripMenuItem.Text = "Sprite Size";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonPencil,
+            this.toolStripButtonEraser,
+            this.toolStripSeparator1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButtonPencil
+            // 
+            this.toolStripButtonPencil.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonPencil.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonPencil.Image")));
+            this.toolStripButtonPencil.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonPencil.Name = "toolStripButtonPencil";
+            this.toolStripButtonPencil.Size = new System.Drawing.Size(43, 22);
+            this.toolStripButtonPencil.Text = "Pencil";
+            this.toolStripButtonPencil.Click += new System.EventHandler(this.toolStripButtonPencil_Click);
+            // 
+            // toolStripButtonEraser
+            // 
+            this.toolStripButtonEraser.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonEraser.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonEraser.Image")));
+            this.toolStripButtonEraser.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonEraser.Name = "toolStripButtonEraser";
+            this.toolStripButtonEraser.Size = new System.Drawing.Size(42, 22);
+            this.toolStripButtonEraser.Text = "Eraser";
+            this.toolStripButtonEraser.Click += new System.EventHandler(this.toolStripButtonEraser_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // pictureBoxCanvas
+            // 
+            this.pictureBoxCanvas.InitialImage = null;
+            this.pictureBoxCanvas.Location = new System.Drawing.Point(260, 49);
+            this.pictureBoxCanvas.Name = "pictureBoxCanvas";
+            this.pictureBoxCanvas.Size = new System.Drawing.Size(528, 389);
+            this.pictureBoxCanvas.TabIndex = 2;
+            this.pictureBoxCanvas.TabStop = false;
+            this.pictureBoxCanvas.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBoxCanvas_MouseClick);
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 49);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(254, 401);
+            this.splitter1.TabIndex = 3;
+            this.splitter1.TabStop = false;
+            // 
+            // canvasVScrollBar
+            // 
+            this.canvasVScrollBar.Location = new System.Drawing.Point(771, 49);
+            this.canvasVScrollBar.Name = "canvasVScrollBar";
+            this.canvasVScrollBar.Size = new System.Drawing.Size(17, 372);
+            this.canvasVScrollBar.TabIndex = 4;
+            // 
+            // canvasHScrollBar
+            // 
+            this.canvasHScrollBar.Location = new System.Drawing.Point(260, 421);
+            this.canvasHScrollBar.Name = "canvasHScrollBar";
+            this.canvasHScrollBar.Size = new System.Drawing.Size(511, 17);
+            this.canvasHScrollBar.TabIndex = 5;
+            // 
+            // LayerList
+            // 
+            this.LayerList.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.LayerList.ImageSize = new System.Drawing.Size(16, 16);
+            this.LayerList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pictureBoxPreview
+            // 
+            this.pictureBoxPreview.Location = new System.Drawing.Point(0, 333);
+            this.pictureBoxPreview.Name = "pictureBoxPreview";
+            this.pictureBoxPreview.Size = new System.Drawing.Size(148, 117);
+            this.pictureBoxPreview.TabIndex = 6;
+            this.pictureBoxPreview.TabStop = false;
+            // 
+            // listViewLayers
+            // 
+            this.listViewLayers.GridLines = true;
+            this.listViewLayers.LargeImageList = this.LayerList;
+            this.listViewLayers.Location = new System.Drawing.Point(0, 49);
+            this.listViewLayers.Name = "listViewLayers";
+            this.listViewLayers.Size = new System.Drawing.Size(254, 278);
+            this.listViewLayers.TabIndex = 7;
+            this.listViewLayers.UseCompatibleStateImageBehavior = false;
+            // 
+            // SpriteDesigner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listViewLayers);
+            this.Controls.Add(this.pictureBoxPreview);
+            this.Controls.Add(this.canvasHScrollBar);
+            this.Controls.Add(this.canvasVScrollBar);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.pictureBoxCanvas);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "SpriteDesigner";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCanvas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPreview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem spriteSizeToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButtonPencil;
+        private System.Windows.Forms.ToolStripButton toolStripButtonEraser;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.PictureBox pictureBoxCanvas;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.VScrollBar canvasVScrollBar;
+        private System.Windows.Forms.HScrollBar canvasHScrollBar;
+        private System.Windows.Forms.ImageList LayerList;
+        private System.Windows.Forms.PictureBox pictureBoxPreview;
+        private System.Windows.Forms.ListView listViewLayers;
+    }
+}
+
---
+++ GameMakingKit/SpriteEditor/SpriteDesigner.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpriteEditor
+{
+    public partial class SpriteDesigner : Form
+    {
+        /// <summary>
+        /// Drawing mode of the designer, true = pencil, false = eraser
+        /// </summary>
+        private bool drawingMode { get; set; } = true;
+
+        public SpriteDesigner()
+        {
+            InitializeComponent();
+            
+        }
+
+
+
+        private void toolStripButtonPencil_Click(object sender, EventArgs e)
+        {
+            drawingMode = true;
+            toolStripButtonPencil.Checked = true;
+            toolStripButtonEraser.Checked = false;
+        }
+
+        private void toolStripButtonEraser_Click(object sender, EventArgs e)
+        {
+            drawingMode = false;
+            toolStripButtonPencil.Checked = false;
+            toolStripButtonEraser.Checked = true;
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+
+        private void pictureBoxCanvas_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+    }
+}
